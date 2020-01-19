e50b898a703a56e34d09999aa27e5184e0e69641
jeremycailloux <59084021+jeremycailloux@users.noreply.github.com>
2020-01-19 15:57:59 +0100

    Add files via upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  isAuth = "false";
+  isAuth = false;
+  constructor() {
+    setTimeout(
+      () => {
+        this.isAuth = true;
+      }, 4000
+    );
+  }
 }
