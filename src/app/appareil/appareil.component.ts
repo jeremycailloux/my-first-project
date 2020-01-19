84de78b03e19a8f0b655787eff7221f0a2f8fb6f
jeremycailloux <59084021+jeremycailloux@users.noreply.github.com>
2020-01-19 15:36:28 +0100

    Add files via upload

---
+++ src/app/appareil/appareil.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-appareil',
+  templateUrl: './appareil.component.html',
+  styleUrls: ['./appareil.component.css']
+})
+export class AppareilComponent implements OnInit {
+
+  appareilName = 'Machine à laver';
+  appareilStatus = 'éteint';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+  getStatus(){
+    return this.appareilStatus;
+  }
+}
